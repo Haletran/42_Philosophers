7eb5e0ad465c2f588be22aa5bc81b04b640512e9
Haletran <bpasquier123@gmail.com>
2024-05-05 22:34:16 +0200

    print error in fd 2

--- philo/src/utils/print_error.c
+++ philo/src/utils/print_error.c
@@ -3,17 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   print_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
+/*   By: baptiste <baptiste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:42:13 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/04/24 17:31:36 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/05/05 22:23:12 by baptiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philo.h"
 
+int	ft_putstr_fd(char *str, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!str || !fd)
+		return (-1);
+	while (*str != 0)
+		i += write(fd, str++, 1);
+	return (i);
+}
+
+
 int	print_error(char *str)
 {
-	printf(RED "Error :" RESET " %s\n", str);
+	ft_putstr_fd(RED "Error :" RESET " ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd("\n", 2);
 	return (FAILURE);
 }
